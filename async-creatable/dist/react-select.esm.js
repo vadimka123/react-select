138a85b4834ce4f78f307c67b892e057d78d9d0d
Vadim Goy <vadim.goy@smoozeedev.com>
2021-03-16 14:00:50 +0200

    new build

---
+++ async-creatable/dist/react-select.esm.js
@@ -0,0 +1,25 @@
+import { makeAsyncSelect } from '../../async/dist/react-select.esm.js';
+import { makeCreatableSelect } from '../../creatable/dist/react-select.esm.js';
+import { m as manageState } from '../../dist/stateManager-76bdb5d6.esm.js';
+import { S as Select } from '../../dist/Select-845440d8.esm.js';
+import '@babel/runtime/helpers/extends';
+import '@babel/runtime/helpers/objectWithoutProperties';
+import '@babel/runtime/helpers/defineProperty';
+import '../../dist/index-046bda7b.esm.js';
+import '@emotion/react';
+import '@babel/runtime/helpers/taggedTemplateLiteral';
+import '@babel/runtime/helpers/typeof';
+import 'react-input-autosize';
+import '@babel/runtime/helpers/classCallCheck';
+import '@babel/runtime/helpers/createClass';
+import '@babel/runtime/helpers/inherits';
+import 'react';
+import 'react-dom';
+import '@babel/runtime/helpers/toConsumableArray';
+import 'memoize-one';
+
+var SelectCreatable = makeCreatableSelect(Select);
+var SelectCreatableState = manageState(SelectCreatable);
+var AsyncCreatable = makeAsyncSelect(SelectCreatableState);
+
+export default AsyncCreatable;
---
+++ base/dist/react-select.cjs.prod.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: !0
+}), require("@babel/runtime/helpers/extends"), require("../../dist/index-cb5ab358.cjs.prod.js"), 
+require("@babel/runtime/helpers/classCallCheck"), require("@babel/runtime/helpers/createClass"), 
+require("@babel/runtime/helpers/inherits"), require("@babel/runtime/helpers/toConsumableArray"), 
+require("react");
+
+var base_dist_reactSelect = require("../../dist/Select-9215081e.cjs.prod.js");
+
+require("@emotion/react"), require("@babel/runtime/helpers/taggedTemplateLiteral"), 
+require("@babel/runtime/helpers/objectWithoutProperties"), require("@babel/runtime/helpers/typeof"), 
+require("react-input-autosize"), require("@babel/runtime/helpers/defineProperty"), 
+require("react-dom"), require("memoize-one"), exports.default = base_dist_reactSelect.Select, 
+exports.defaultProps = base_dist_reactSelect.defaultProps;
